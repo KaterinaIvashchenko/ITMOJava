c1e0a25669a9ce5ff35a5e4817b52562ad284dee
katerina ivashchenko <ivashchenko.ki@gmail.com>
2016-10-26 16:43:30 +0300

    delenie chisel

---
+++ ExOne/src/ExOne.java
@@ -0,0 +1,12 @@
+/**
+ * Created by katerina on 10/26/16.
+ */
+public class ExOne {
+    public static void main(String[] args) {
+        int q, w, r;
+        q = 21;
+        w = 8;
+        r = q / w;
+        System.out.println(q + " / " + w + " = " + r + " и " + (q - (r * w)) + " в остатке");
+    }
+}
