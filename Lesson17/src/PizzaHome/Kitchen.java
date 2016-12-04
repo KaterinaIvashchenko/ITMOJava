1d0705b5565a4d869ad599e06c8c6e4c600c076a
katerina ivashchenko <ivashchenko.ki@gmail.com>
2016-12-04 19:07:58 +0300

    Lesson17 - Builder for PizzaHome

--- Lesson17/src/PizzaHome/Kitchen.java
+++ Lesson17/src/PizzaHome/Kitchen.java
@@ -8,8 +8,8 @@ public class Kitchen {
 
     public static void main(String[] args) {
 
-        Pizza Katerita = new Pizza.Cook(1).yummy("Katerita").eggs(6).chicken(1).mushrooms(12).tomatoes(4).cook();
+        Pizza katerita = new Pizza.Cook(1).yummy("Katerita").eggs(6).chicken(1).mushrooms(12).tomatoes(4).cook();
 
-        System.out.println(Katerita);
+        System.out.println(katerita);
     }
 }
